bc593e6967a8e8af806fc701d791c24de658ab2a
Gregory Neagu <neagu@cs.ualberta.ca>
2018-09-25 19:10:49 -0600

    TEDAZXS

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
@@ -1,10 +1,6 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import java.util.ArrayList;
-
 public class NormalTweet extends Tweet {
-
-    private ArrayList<String> MoodList = new ArrayList<String>();
     NormalTweet() {
         super();
     }
