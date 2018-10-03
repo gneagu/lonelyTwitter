e44c0490a7936ca70f78666080e4879206b6f1e3
Gregory Neagu <neagu@cs.ualberta.ca>
2018-10-02 19:01:19 -0600

    Documented

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * A simple twitter client that takes comments of 140 characters or less,
+ * and prints them to the screen. Valid comments are saved on the disk in
+ * chronological order.
+ *
+ * @author: Gregory Neagu
+ * @since: 1.0
+ * @throws: TweetTooLongException
+ */
+package ca.ualberta.cs.lonelytwitter;
